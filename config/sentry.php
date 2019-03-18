d477ecac2a7bfea5e9bd4ab9333ef32bb082244f
Fabio.ROCCO <fabio.rocco@green-conseil.com>
2019-03-18 03:19:10 +0100

    Sentry:: use sentry for logs

---
+++ config/sentry.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'dsn' => 'https://324e115e4f0d42f58f3c9ce2b7bcd68a@sentry.io/1417059',
+
+	// capture release as git sha
+	// 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
+);
